ca879a844c5415b60914bc04f454b63c52ba6603
Bayuaji64 <bayuajipradipta64@gmail.com>
2023-07-30 18:48:44 +0700

    feat:done creat from, error and success message

--- task_list/resources/views/layouts/app.blade.php
+++ task_list/resources/views/layouts/app.blade.php
@@ -4,14 +4,23 @@
 <head>
     <meta charset="UTF-8">
     <title>Task List App With Laravel 10</title>
+    @yield('style')
 </head>
 
 <body>
 
     <h1>@yield('title')</h1>
     <div>
+        @if (session()-> has('success'))
+
+        <div>{{session('success')}}</div>
+            
+        @endif
         @yield('content')
     </div>
+    {{-- <div>
+        @yield('content')
+    </div> --}}
 
 </body>
 
